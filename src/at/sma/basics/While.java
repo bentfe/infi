f401bb00eb8394c9e06247f87aa8e75b0c96ebd3
bentfe <bent.fellacher@student.htldornbirn.at>
2023-10-18 11:25:09 +0200

    Readme File erstellt

--- src/at/sma/basics/While.java
+++ src/at/sma/basics/While.java
@@ -7,10 +7,10 @@ public class While {
     public static void main(String[] args) {
         boolean stop = false;
         while (stop==false) {
-            Random random= new Random();
+            Random random = new Random();
             int randomNumber = random.nextInt(100);
             System.out.println(randomNumber);
-            if (randomNumber == 15 || randomNumber==25) {
+            if (randomNumber == 15 || randomNumber == 25) {
                 stop = true;
             }
         }
