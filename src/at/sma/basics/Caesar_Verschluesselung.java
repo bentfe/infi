88e7617970ffef58318f5ef6bf9405bdee070741
bentfe <bent.fellacher@student.htldornbirn.at>
2023-11-08 11:30:28 +0100

    Bankomat Funktionen geändert

---
+++ src/at/sma/basics/Caesar_Verschluesselung.java
@@ -0,0 +1,15 @@
+package at.sma.basics;
+
+import java.util.Random;
+
+public class Caesar_Verschluesselung {
+    public static void main(String[] args) {
+        String result = encrypt(1, "hallo");
+    }
+
+    public static String encrypt(int shift, String stringToEncrypt) {
+        char[] inputdata = stringToEncrypt.toCharArray();
+
+        return "";
+    }
+}
