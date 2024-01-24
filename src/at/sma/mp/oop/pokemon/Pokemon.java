e77a7ebb20a1b1a08e812f0dce33676f71ea211f
bentfe <bent.fellacher@student.htldornbirn.at>
2024-01-24 11:28:20 +0100

    abgabe

--- src/at/sma/mp/oop/pokemon/Pokemon.java
+++ src/at/sma/mp/oop/pokemon/Pokemon.java
@@ -1,11 +1,22 @@
 package at.sma.mp.oop.pokemon;
 
+import at.sma.basics.struct.Switch;
+import at.sma.mp.oop.auto.Auto;
+
+import java.util.ArrayList;
+
+import java.util.Random;
+
+
 public class Pokemon {
-    public String typ;
-    public int level;
-    public String zweiterTyp;
-    public int alter;
-    public int groesse;
+    private String pokemon;
+    private int level;
+    private String typ;
+    private int alter;
+    private int groesse;
+    private ArrayList<Pokemon> pokemonListe;
+    Random r = new Random();
+
 
     public String getTyp() {
         return typ;
@@ -23,12 +34,12 @@ public class Pokemon {
         this.level = level;
     }
 
-    public String getZweiterTyp() {
-        return zweiterTyp;
+    public String getPokemon() {
+        return pokemon;
     }
 
-    public void setZweiterTyp(String zweiterTyp) {
-        this.zweiterTyp = zweiterTyp;
+    public void setPokemon(String pokemon) {
+        this.pokemon = pokemon;
     }
 
     public int getAlter() {
@@ -47,26 +58,69 @@ public class Pokemon {
         this.groesse = groesse;
     }
 
-    public Pokemon(String typ, int level, String zweiterTyp, int alter, int groesse) {
+    public ArrayList<Pokemon> getPokemonListe() {
+        return pokemonListe;
+    }
+
+    public void setPokemonListe(ArrayList<Pokemon> pokemonListe) {
+        this.pokemonListe = pokemonListe;
+    }
+
+    public Pokemon(String pokemon, String typ, int level, int alter, int groesse) {
+        this.pokemon = pokemon;
         this.typ = typ;
         this.level = level;
-        this.zweiterTyp = zweiterTyp;
         this.alter = alter;
         this.groesse = groesse;
     }
 
-    public String info {
-        if (this.getZweiterTyp() == "") {
-            System.out.println("Das Pokemon ist vom Typ " + this.getTyp()
-            + ", ist auf dem Level " + this.getLevel()
-            + ", ist " + this.getAlter() + " Jahre alt und ist "
-            + this.getGroesse() + " cm groß.");
-        } else {
-            System.out.println("Das Pokemon ist vom Typ " + this.getTyp()
-                    + ", hat den sekundären Typ " + this.getZweiterTyp()
-                    + ", ist auf dem Level " + this.getLevel()
-                    + ", ist " + this.getAlter() + " Jahre alt und ist "
-                    + this.getGroesse() + " cm groß.");
+    public Pokemon() {
+        this.pokemon = "";
+        this.typ = "";
+        this.level = 0;
+        this.alter = 0;
+        this.groesse = 0;
+    }
+
+    public String info
+
+    {
+        System.out.println("Das Pokemon" + this.getPokemon()
+                + " ist vom Typ " + this.getTyp()
+                + ", ist auf dem Level " + this.getLevel()
+                + ", ist " + this.getAlter() + " Jahre alt und ist "
+                + this.getGroesse() + " cm groß.");
+    }
+
+    public Pokemon(int anzahl) {
+        String[] aPokemons = {"Pikachu", "Glumanda", "Bisasam", "Schiggy", "Mew", "Serpifeu", "Woingenau"};
+        for (var i = 0; i <= anzahl; i++) {
+            int level = r.nextInt(100) + 1;
+            int groesse = r.nextInt() + 1;
+            int iPokemon = r.nextInt(aPokemons.length);
+            String pokemonName = aPokemons[iPokemon];
+            int alter = r.nextInt(40) + 1;
+            String typ = "";
+            switch (pokemonName) {
+                case "Pikachu":
+                    typ = "Elektro";
+                    break;
+                case "Glumanda":
+                    typ = "Feuer";
+                    break;
+                case "Bisasam", "Serpifeu":
+                    typ = "Pflanze";
+                    break;
+                case "Schiggy":
+                    typ = "Wasser";
+                    break;
+                case "Mew", "Woingenau":
+                    typ = "Psycho";
+                    break;
+                default:
+            }
+            this.pokemonListe.add(new Pokemon(pokemonName,typ,level,alter,groesse));
         }
     }
 }
+
--- src/at/sma/mp/oop/pokemon/Testit.java
+++ src/at/sma/mp/oop/pokemon/Testit.java
@@ -1,4 +1,7 @@
 package at.sma.mp.oop.pokemon;
 
 public class Testit {
+    public static void main(String[] args) {
+        Pokemon a = new Pokemon(100)
+    }
 }
